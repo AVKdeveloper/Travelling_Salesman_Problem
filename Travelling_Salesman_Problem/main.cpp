192f225bf9fd4e6cd715decdbb79efd4f4829afe
AVKdeveloper <kosov.programming@gmail.com>
2016-11-13 23:37:04 +0300

    Working version

--- Travelling_Salesman_Problem/main.cpp
+++ Travelling_Salesman_Problem/main.cpp
@@ -10,6 +10,7 @@
 #include <cstdlib>
 #include <iostream>
 #include <numeric>
+#include <cassert>
 #include "graph_for_mst.h"
 #include "box-muller_transform.h"
 #include "precise_solution_for_tsp.h"
@@ -17,21 +18,26 @@
 int main()
 {
 	const int kMaxVerticesQuantity = 10;
-	const int kTestQuantity = 1;
+	const int kTestQuantity = 10;
+	const bool kDetailedReport = false; // Write datils of every test in console? 
 
 	std::fstream file;
 	file.open("Statistics.out", std::fstream::out);
 	file << "Here is statistics on comparing the precise answer and approaching answer:\n";
-	file << "Quantity of points   Average precise answer   Average approaching answer   Accuracy\n";
-	for (int vertices_quantity = 2; vertices_quantity < kMaxVerticesQuantity; ++vertices_quantity) { // for different quantities of vertices
+	file << "Quantity of points Average precise answer Average approaching answer Difference\n";
+	for (int vertices_quantity = 2; vertices_quantity <= kMaxVerticesQuantity; ++vertices_quantity) { // for different quantities of vertices
 		std::vector<double> precise_answers(kTestQuantity);
 		std::vector<double> approaching_answers(kTestQuantity);
 		for (int test_number = 0; test_number < kTestQuantity; ++test_number) {
 			std::vector<std::pair<double, double>> points(vertices_quantity);
-			std::cout << "Point for vertices quantity = " << vertices_quantity << " test No" << test_number << ":\n";
+			if (kDetailedReport) {
+				std::cout << "Point for vertices quantity = " << vertices_quantity << " test No" << test_number << ":\n";
+			}
 			for (int i = 0; i < vertices_quantity; ++i) { // generating random points 
 				points[i] = BoxMullerTransformation<double>(1 - rand()/((double)RAND_MAX + 1), 1 - rand() / ((double)RAND_MAX + 1));
-				std::cout << "(" << points[i].first << " ; " << points[i].second << ")  ";
+				if (kDetailedReport) {
+					std::cout << "(" << points[i].first << " ; " << points[i].second << ")  ";
+				}
 			} // generated random points
 			std::cout << std::endl;
 
@@ -53,47 +59,23 @@ int main()
 			for (Edge<double> edge : mst_edges) {
 				sum_of_weights += edge.weight_;
 			} // counted the sum of weights og edges in mst tree
-			double precise_answer = FindBestHamiltonianCycle(points);
-			std::cout << "Approaching answer(using mst tree): " << 2*sum_of_weights << "\n";
-			std::cout << "Precise answer(using DFS): " << precise_answer << "\n\n";
+			double precise_answer = FindBestHamiltonianCycle(points); 
+			if (kDetailedReport) {
+				std::cout << "Approaching answer(using mst tree): " << 2 * sum_of_weights << "\n";
+				std::cout << "Precise answer(using DFS): " << precise_answer << "\n\n";
+			}
+			assert(2 * sum_of_weights >= precise_answer); // check that the best answer is better than approaching
 			approaching_answers[test_number] = 2 * sum_of_weights;
 			precise_answers[test_number] = precise_answer;
 		}
 		double average_precise_answer = std::accumulate(precise_answers.begin(), precise_answers.end(), 0.0) / precise_answers.size();
 		double average_approaching_answer = std::accumulate(approaching_answers.begin(), approaching_answers.end(), 0.0) / approaching_answers.size();
-		file << vertices_quantity << "  " << average_precise_answer << "  " << average_approaching_answer <<
-			    "  " << (average_approaching_answer - average_precise_answer) / average_precise_answer << "\n";
+		file << vertices_quantity << "                  " << average_precise_answer << "                " << average_approaching_answer <<
+			    "                    " << (average_approaching_answer - average_precise_answer) / average_precise_answer << " % \n";
 	}
 	file.close();
-
-	system("pause");
-
-
-	/*
-	std::fstream file;
-	file.open("kruskal.in", std::fstream::in);
-	int number_of_vertices, number_of_edges;
-	file >> number_of_vertices >> number_of_edges; // getting init info from file
-	GraphForMst<int> graph(number_of_vertices, number_of_edges);
-	for (int i = 0; i < number_of_edges; ++i) {
-		Edge<int> edge;
-		file >> edge.from_ >> edge.to_ >> edge.weight_;
-		edge.from_--;
-		edge.to_--;
-		graph.AddEdge(edge);
-	} // end of init graph
-	file.close();
-
-	//start of the Boruvka's algorithm ->
-	std::unordered_set<Edge<int>> mst_tree = graph.BuiltMstByBoruvka();
-	long long sum_of_weights = 0;
-	for (Edge<int> edge : mst_tree) {
-		sum_of_weights += edge.weight_;
-	} // counted the sum - answer for owr task
-
-	file.open("kruskal.out", std::fstream::out);
-	file << sum_of_weights; // output
-	file.close();
-	*/
+	if (kDetailedReport) {
+		system("pause");
+	}
 	return 0;
 }
